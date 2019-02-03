5d890ec479ac49ef4b4068cd5cb19710b2906a52
jinhoon <g22203@naver.com>
2019-02-03 16:08:58 +0900

    update by jh

---
+++ HadicapedNavigation-master/SinTest/app/src/main/java/com/example/inwif/sintest/TrainingMode.java
@@ -0,0 +1,31 @@
+package com.example.inwif.sintest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.WindowManager;
+
+public class TrainingMode extends AppCompatActivity {
+    private Runnable frameCapture;
+    private Thread startCapture;
+    private SurfaceView surfaceView;
+    private CameraPreview mCameraPreview;
+    private View mLayout;  // Snackbar 사용하기 위해서는 View가 필요합니다.
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // 상태바를 안보이도록 합니다.
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        // 화면 켜진 상태를 유지합니다.
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON,
+                WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        setContentView(R.layout.activity_training_mode);
+
+        mLayout = findViewById(R.id.layout_main);
+        surfaceView = findViewById(R.id.camera_preview_main);
+
+    }
+}
